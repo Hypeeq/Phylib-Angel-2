1f13711711fba8548b8242a40e2e2c782bcce9da
Hypeeq <shreeharshjadhav79@gmail.com>
2024-02-14 05:16:46 -0500

    initial

---
+++ Physics.py
@@ -0,0 +1,160 @@
+import phylib;
+
+################################################################################
+# import constants from phylib to global varaibles
+BALL_RADIUS   = phylib.PHYLIB_BALL_RADIUS;
+
+# add more here
+
+################################################################################
+# the standard colours of pool balls
+# if you are curious check this out:  
+# https://billiards.colostate.edu/faq/ball/colors/
+
+BALL_COLOURS = [ 
+    "WHITE",
+    "YELLOW",
+    "BLUE",
+    "RED",
+    "PURPLE",
+    "ORANGE",
+    "GREEN",
+    "BROWN",
+    "BLACK",
+    "LIGHTYELLOW",
+    "LIGHTBLUE",
+    "PINK",             # no LIGHTRED
+    "MEDIUMPURPLE",     # no LIGHTPURPLE
+    "LIGHTSALMON",      # no LIGHTORANGE
+    "LIGHTGREEN",
+    "SANDYBROWN",       # no LIGHTBROWN 
+    ];
+
+################################################################################
+class Coordinate( phylib.phylib_coord ):
+    """
+    This creates a Coordinate subclass, that adds nothing new, but looks
+    more like a nice Python class.
+    """
+    pass;
+
+
+################################################################################
+class StillBall( phylib.phylib_object ):
+    """
+    Python StillBall class.
+    """
+
+    def __init__( self, number, pos ):
+        """
+        Constructor function. Requires ball number and position (x,y) as
+        arguments.
+        """
+
+        # this creates a generic phylib_object
+        phylib.phylib_object.__init__( self, 
+                                       phylib.PHYLIB_STILL_BALL, 
+                                       number, 
+                                       pos, None, None, 
+                                       0.0, 0.0 );
+      
+        # this converts the phylib_object into a StillBall class
+        self.__class__ = StillBall;
+
+
+    # add an svg method here
+
+
+################################################################################
+
+class Table( phylib.phylib_table ):
+    """
+    Pool table class.
+    """
+
+    def __init__( self ):
+        """
+        Table constructor method.
+        This method call the phylib_table constructor and sets the current
+        object index to -1.
+        """
+        phylib.phylib_table.__init__( self );
+        self.current = -1;
+
+    def __iadd__( self, other ):
+        """
+        += operator overloading method.
+        This method allows you to write "table+=object" to add another object
+        to the table.
+        """
+        self.add_object( other );
+        return self;
+
+    def __iter__( self ):
+        """
+        This method adds iterator support for the table.
+        This allows you to write "for object in table:" to loop over all
+        the objects in the table.
+        """
+        return self;
+
+    def __next__( self ):
+        """
+        This provides the next object from the table in a loop.
+        """
+        self.current += 1;  # increment the index to the next object
+        if self.current < MAX_OBJECTS:   # check if there are no more objects
+            return self[ self.current ]; # return the latest object
+
+        # if we get there then we have gone through all the objects
+        self.current = -1;    # reset the index counter
+        raise StopIteration;  # raise StopIteration to tell for loop to stop
+
+    def __getitem__( self, index ):
+        """
+        This method adds item retreivel support using square brackets [ ] .
+        It calls get_object (see phylib.i) to retreive a generic phylib_object
+        and then sets the __class__ attribute to make the class match
+        the object type.
+        """
+        result = self.get_object( index ); 
+        if result==None:
+            return None;
+        if result.type == phylib.PHYLIB_STILL_BALL:
+            result.__class__ = StillBall;
+        if result.type == phylib.PHYLIB_ROLLING_BALL:
+            result.__class__ = RollingBall;
+        if result.type == phylib.PHYLIB_HOLE:
+            result.__class__ = Hole;
+        if result.type == phylib.PHYLIB_HCUSHION:
+            result.__class__ = HCushion;
+        if result.type == phylib.PHYLIB_VCUSHION:
+            result.__class__ = VCushion;
+        return result;
+
+    def __str__( self ):
+        """
+        Returns a string representation of the table that matches
+        the phylib_print_table function from A1Test1.c.
+        """
+        result = "";    # create empty string
+        result += "time = %6.1f;\n" % self.time;    # append time
+        for i,obj in enumerate(self): # loop over all objects and number them
+            result += "  [%02d] = %s\n" % (i,obj);  # append object description
+        return result;  # return the string
+
+    def segment( self ):
+        """
+        Calls the segment method from phylib.i (which calls the phylib_segment
+        functions in phylib.c.
+        Sets the __class__ of the returned phylib_table object to Table
+        to make it a Table object.
+        """
+
+        result = phylib.phylib_table.segment( self );
+        if result:
+            result.__class__ = Table;
+            result.current = -1;
+        return result;
+
+    # add svg method here
---
+++ makefile
@@ -0,0 +1,17 @@
+CC = clang
+CFLAGS = -std=c99 -Wall -pedantic
+LDFLAGS = -L. -lphylib -lm
+
+all: libphylib.so 
+
+phylib.o: phylib.c phylib.h
+	$(CC) $(CFLAGS) -fpic -c $< -o $@
+
+libphylib.so: phylib.o
+	$(CC) -shared -o $@ $<
+
+A1test1: A1test1.c phylib.h libphylib.so
+	$(CC) $(CFLAGS) $< $(LDFLAGS) -o $@
+
+clean:
+	rm -f *.o *.so 
