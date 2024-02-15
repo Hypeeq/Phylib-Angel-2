1705070a0f8512620a3901c7e08eae05ee2c351f
Hypeeq <shreeharshjadhav79@gmail.com>
2024-02-15 06:22:22 -0500

    red

--- Physics.py
+++ Physics.py
@@ -1,27 +1,16 @@
-import phylib;
-import _phylib;
+import phylib
 
-################################################################################
-# import constants from phylib to global varaibles
-PHYLIB_BALL_DIAMETER = phylib.PHYLIB_BALL_DIAMETER;
-PHYLIB_HOLE_RADIUS = phylib.PHYLIB_HOLE_RADIUS;
-PHYLIB_TABLE_LENGTH = phylib.PHYLIB_TABLE_LENGTH;
-PHYLIB_TABLE_WIDTH = phylib.PHYLIB_TABLE_WIDTH;
-PHYLIB_SIM_RATE = phylib.PHYLIB_SIM_RATE;
-PHYLIB_VEL_EPSILON = phylib.PHYLIB_VEL_EPSILON;
-PHYLIB_DRAG = phylib.PHYLIB_DRAG;
-PHYLIB_MAX_TIME = phylib.PHYLIB_MAX_TIME;
-PHYLIB_MAX_OBJECTS = phylib.PHYLIB_MAX_OBJECTS;
+PHYLIB_BALL_DIAMETER = phylib.PHYLIB_BALL_DIAMETER
+PHYLIB_HOLE_RADIUS = phylib.PHYLIB_HOLE_RADIUS
+PHYLIB_TABLE_LENGTH = phylib.PHYLIB_TABLE_LENGTH
+PHYLIB_TABLE_WIDTH = phylib.PHYLIB_TABLE_WIDTH
+PHYLIB_SIM_RATE = phylib.PHYLIB_SIM_RATE
+PHYLIB_VEL_EPSILON = phylib.PHYLIB_VEL_EPSILON
+PHYLIB_DRAG = phylib.PHYLIB_DRAG
+PHYLIB_MAX_TIME = phylib.PHYLIB_MAX_TIME
+PHYLIB_MAX_OBJECTS = phylib.PHYLIB_MAX_OBJECTS
 
-
-# add more here
-
-################################################################################
-# the standard colours of pool balls
-# if you are curious check this out:  
-# https://billiards.colostate.edu/faq/ball/colors/
-
-BALL_COLOURS = [ 
+BALL_COLOURS = [
     "WHITE",
     "YELLOW",
     "BLUE",
@@ -33,49 +22,42 @@ BALL_COLOURS = [
     "BLACK",
     "LIGHTYELLOW",
     "LIGHTBLUE",
-    "PINK",             # no LIGHTRED
-    "MEDIUMPURPLE",     # no LIGHTPURPLE
-    "LIGHTSALMON",      # no LIGHTORANGE
+    "PINK",
+    "MEDIUMPURPLE",
+    "LIGHTSALMON",
     "LIGHTGREEN",
-    "SANDYBROWN",       # no LIGHTBROWN 
-    ];
+    "SANDYBROWN",
+]
 
-################################################################################
-class Coordinate( phylib.phylib_coord ):
+class Coordinate(phylib.phylib_coord):
     """
     This creates a Coordinate subclass, that adds nothing new, but looks
     more like a nice Python class.
     """
-    pass;
+    pass
 
 
-################################################################################
-class StillBall( phylib.phylib_object ):
+class StillBall(phylib.phylib_object):
     """
     Python StillBall class.
     """
 
-    def __init__( self, number, pos ):
-        """
-        Constructor function. Requires ball number and position (x,y) as
-        arguments.
-        """
+    def __init__(self, number, pos):
+        phylib.phylib_object.__init__(
+            self,
+            phylib.PHYLIB_STILL_BALL,
+            number,
+            pos,
+            None,
+            None,
+            0.0,
+            0.0,
+        )
+        self.__class__ = StillBall
 
-        # this creates a generic phylib_object
-        phylib.phylib_object.__init__( self, 
-                                       phylib.PHYLIB_STILL_BALL, 
-                                       number, 
-                                       pos, None, None, 
-                                       0.0, 0.0 );
-      
-        # this converts the phylib_object into a StillBall class
-        self.__class__ = StillBall;
+    def svg(self):
+        return f"<circle cx='{self.pos.x}' cy='{self.pos.y}' r='{PHYLIB_BALL_DIAMETER/2}' fill='{BALL_COLOURS[self.number]}' />\n"
 
-
-    # add an svg method here
-
-
-################################################################################
 
 class RollingBall(phylib.phylib_object):
     """
@@ -83,19 +65,20 @@ class RollingBall(phylib.phylib_object):
     """
 
     def __init__(self, number, pos, vel, acc):
-        """
-        Constructor function. Requires ball number, position (x,y), velocity (vx,vy),
-        and acceleration (ax,ay) as arguments.
-        """
-        phylib.phylib_object.__init__(self,
-                                       phylib.PHYLIB_ROLLING_BALL,
-                                       number,
-                                       pos,
-                                       None, None,
-                                       vel,
-                                       acc);
+        phylib.phylib_object.__init__(
+            self,
+            phylib.PHYLIB_ROLLING_BALL,
+            number,
+            pos,
+            None,
+            None,
+            vel,
+            acc,
+        )
+        self.__class__ = RollingBall
 
-        self.__class__ = RollingBall;
+    def svg(self):
+        return f"<circle cx='{self.pos.x}' cy='{self.pos.y}' r='{PHYLIB_BALL_DIAMETER/2}' fill='{BALL_COLOURS[self.number]}' />\n"
 
 
 class Hole(phylib.phylib_object):
@@ -104,16 +87,13 @@ class Hole(phylib.phylib_object):
     """
 
     def __init__(self, pos):
-        """
-        Constructor function. Requires position (x,y) as argument.
-        """
-        phylib.phylib_object.__init__(self,
-                                       phylib.PHYLIB_HOLE,
-                                       None,
-                                       pos,
-                                       None, None, None, None);
+        phylib.phylib_object.__init__(
+            self, phylib.PHYLIB_HOLE, None, pos, None, None, None, None
+        )
+        self.__class__ = Hole
 
-        self.__class__ = Hole;
+    def svg(self):
+        return f"<circle cx='{self.pos.x}' cy='{self.pos.y}' r='{PHYLIB_HOLE_RADIUS}' fill='black' />\n"
 
 
 class HCushion(phylib.phylib_object):
@@ -122,17 +102,16 @@ class HCushion(phylib.phylib_object):
     """
 
     def __init__(self, y):
-        """
-        Constructor function. Requires y-coordinate as argument.
-        """
-        phylib.phylib_object.__init__(self,
-                                       phylib.PHYLIB_HCUSHION,
-                                       None,
-                                       None,
-                                       None, None, None, None);
+        phylib.phylib_object.__init__(
+            self, phylib.PHYLIB_HCUSHION, None, None, None, None, None, None
+        )
+        self.obj.hcushion.y = y
+        self.__class__ = HCushion
 
-        self.obj.hcushion.y = y;
-        self.__class__ = HCushion;
+    def svg(self):
+        return (
+            f"<rect width='1400' height='25' x='-25' y='{self.obj.hcushion.y}' fill='darkgreen' />\n"
+        )
 
 
 class VCushion(phylib.phylib_object):
@@ -141,107 +120,83 @@ class VCushion(phylib.phylib_object):
     """
 
     def __init__(self, x):
-        """
-        Constructor function. Requires x-coordinate as argument.
-        """
-        phylib.phylib_object.__init__(self,
-                                       phylib.PHYLIB_VCUSHION,
-                                       None,
-                                       None,
-                                       None, None, None, None);
+        phylib.phylib_object.__init__(
+            self, phylib.PHYLIB_VCUSHION, None, None, None, None, None, None
+        )
+        self.obj.vcushion.x = x
+        self.__class__ = VCushion
 
-        self.obj.vcushion.x = x;
-        self.__class__ = VCushion;
+    def svg(self):
+        return (
+            f"<rect width='25' height='2750' x='{self.obj.vcushion.x}' y='-25' fill='darkgreen' />\n"
+        )
 
 
-class Table( phylib.phylib_table ):
+class Table(phylib.phylib_table):
     """
     Pool table class.
     """
 
-    def __init__( self ):
-        """
-        Table constructor method.
-        This method call the phylib_table constructor and sets the current
-        object index to -1.
-        """
-        phylib.phylib_table.__init__( self );
-        self.current = -1;
+    def __init__(self):
+        phylib.phylib_table.__init__(self)
+        self.current = -1
 
-    def __iadd__( self, other ):
-        """
-        += operator overloading method.
-        This method allows you to write "table+=object" to add another object
-        to the table.
-        """
-        self.add_object( other );
-        return self;
+    def __iadd__(self, other):
+        self.add_object(other)
+        return self
 
-    def __iter__( self ):
-        """
-        This method adds iterator support for the table.
-        This allows you to write "for object in table:" to loop over all
-        the objects in the table.
-        """
-        return self;
+    def __iter__(self):
+        return self
 
-    def __next__( self ):
-        """
-        This provides the next object from the table in a loop.
-        """
-        self.current += 1;  # increment the index to the next object
-        if self.current < MAX_OBJECTS:   # check if there are no more objects
-            return self[ self.current ]; # return the latest object
+    def __next__(self):
+        self.current += 1
+        if self.current < PHYLIB_MAX_OBJECTS:
+            return self[self.current]
+        self.current = -1
+        raise StopIteration
 
-        # if we get there then we have gone through all the objects
-        self.current = -1;    # reset the index counter
-        raise StopIteration;  # raise StopIteration to tell for loop to stop
-
-    def __getitem__( self, index ):
-        """
-        This method adds item retreivel support using square brackets [ ] .
-        It calls get_object (see phylib.i) to retreive a generic phylib_object
-        and then sets the __class__ attribute to make the class match
-        the object type.
-        """
-        result = self.get_object( index ); 
-        if result==None:
-            return None;
+    def __getitem__(self, index):
+        result = self.get_object(index)
+        if result == None:
+            return None
         if result.type == phylib.PHYLIB_STILL_BALL:
-            result.__class__ = StillBall;
+            result.__class__ = StillBall
         if result.type == phylib.PHYLIB_ROLLING_BALL:
-            result.__class__ = RollingBall;
+            result.__class__ = RollingBall
         if result.type == phylib.PHYLIB_HOLE:
-            result.__class__ = Hole;
+            result.__class__ = Hole
         if result.type == phylib.PHYLIB_HCUSHION:
-            result.__class__ = HCushion;
+            result.__class__ = HCushion
         if result.type == phylib.PHYLIB_VCUSHION:
-            result.__class__ = VCushion;
-        return result;
+            result.__class__ = VCushion
+        return result
 
-    def __str__( self ):
-        """
-        Returns a string representation of the table that matches
-        the phylib_print_table function from A1Test1.c.
-        """
-        result = "";    # create empty string
-        result += "time = %6.1f;\n" % self.time;    # append time
-        for i,obj in enumerate(self): # loop over all objects and number them
-            result += "  [%02d] = %s\n" % (i,obj);  # append object description
-        return result;  # return the string
+    def __str__(self):
+        result = ""
+        result += "time = %6.1f;\n" % self.time
+        for i, obj in enumerate(self):
+            result += "  [%02d] = %s\n" % (i, obj)
+        return result
 
-    def segment( self ):
-        """
-        Calls the segment method from phylib.i (which calls the phylib_segment
-        functions in phylib.c.
-        Sets the __class__ of the returned phylib_table object to Table
-        to make it a Table object.
-        """
-
-        result = phylib.phylib_table.segment( self );
+    def segment(self):
+        result = phylib.phylib_table.segment(self)
         if result:
-            result.__class__ = Table;
-            result.current = -1;
-        return result;
+            result.__class__ = Table
+            result.current = -1
+        return result
 
-    # add svg method here
+    def svg(self):
+        svg_str = "<svg xmlns='http://www.w3.org/2000/svg' width='1400' height='2750'>\n"
+        for obj in self:
+            svg_str += obj.svg()
+        svg_str += "</svg>"
+        return svg_str
+
+
+# Implement A2Test2.py to write the svg output to a file
+if __name__ == "__main__":
+    table = Table()
+    # Add objects to the table here
+    # Example: table += StillBall(number, Coordinate(x, y))
+    with open("table-0.svg", "w") as file:
+        file.write(table.svg())
